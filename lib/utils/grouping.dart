2533391ccace2973c033d23cb314b44fefd9afc5
Vojtech Pavlovsky <vojtech.pavlovsky@gmail.com>
2020-03-14 11:19:32 +0100

    Add license notice to source files

--- lib/utils/grouping.dart
+++ lib/utils/grouping.dart
@@ -1,3 +1,21 @@
+/*
+ * Metrify: Track Your Metrics
+ * Copyright (C) 2020  Vojtech Pavlovsky
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import 'package:intl/intl.dart';
 import 'package:metrify/models/entry.dart';
 import 'package:metrify/models/group.dart';
